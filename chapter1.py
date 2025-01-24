f1f0718b5fcc5ceb0059c934116925f061d895a3
Pham Hoang Minh - AI Engineer Lead <minh.pham@ndsvn.vn>
2025-01-24 17:13:07 +0700

    updates

--- chapter1.py
+++ chapter1.py
@@ -42,4 +42,125 @@ print("b and w after training: ", b, w)
 linr = LinearRegression()
 linr.fit(x_train, y_train)
 print("b and w from linear regression: ", linr.intercept_, linr.coef_[0])
-#fig = figure3(x_train, y_train)
+fig = figure3(x_train, y_train)
+
+# Test pytorch
+scalar = torch.tensor(3.14159)
+vector = torch.tensor([1,2,3])
+matrix = torch.ones((2,3), dtype=torch.float)
+tensor = torch.randn((2,3,4), dtype=torch.float)
+print("Torch tensors: ", scalar, vector, matrix, tensor)
+print("Tensor size: ", tensor.size(), tensor.shape)
+print("Scalar size: ", scalar.size(), scalar.shape)
+
+# view() means reshape, but creates no new tensor (the resulting one is still the old one with different shape))
+same_matrix = matrix.view(1, 6)
+same_matrix[0, 1] = 2
+print("Old matrix:", matrix)
+print("Reshaped matrix:", same_matrix)
+
+# clone new matrix
+different_matrix = matrix.view(1, 6).clone().detach()
+different_matrix[0, 1] = 3 
+print("Old matrix: ", matrix)
+print("Cloned matrix: ", different_matrix)
+
+# move linear regression from numpy to tensor
+x_train_tensor = torch.as_tensor(x_train) # note: if we modify tensor, we are modifying the array too
+print("Numpy type: ", x_train.dtype)
+print("Torch type: ", x_train_tensor.dtype)
+float_tensor = x_train_tensor.float()
+print("Convert float64 to float32 result: ", float_tensor.dtype)
+
+# torch.as_tensor(): if we modify the array, we are modifying the tensor too
+# torch.tensor() only create a copy
+dummy_array = np.array([1, 2, 3])
+dummy_tensor = torch.as_tensor(dummy_array)
+dummy_tensor_copy = torch.tensor(dummy_array)
+dummy_array[1] = 0
+print("tensor.as_tensor() when array has been changed: ", dummy_tensor)
+print("tensor.tensor() when array has been changed: ", dummy_tensor_copy)
+
+# convert tensor back to numpy 
+returned_array = dummy_tensor.numpy()
+print("Returned array: ", returned_array)
+
+# check if cuda is available
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("Device: ", device)
+n_cudas = torch.cuda.device_count()
+for i in range(n_cudas):
+    print("Device name: ", torch.cuda.get_device_name(i))
+
+# send training data to gpu if available
+gpu_tensor = torch.as_tensor(x_train).to(device)
+print("gpu_tensor shape:", gpu_tensor.shape)
+
+# convert training data to tensor and send to gpu
+x_train_tensor = torch.as_tensor(x_train).float().to(device)
+y_train_tensor = torch.as_tensor(y_train).float().to(device)
+print("Types of array and tensors: ", type(x_train), type(x_train_tensor), x_train_tensor.type())
+
+# Note: numpy() cannot convert gpu-tensor, must convert to cpu-tensor first
+back_to_numpy = x_train_tensor.cpu().numpy()
+print("Back to numpy result: ", back_to_numpy)
+
+# For trainable parameters
+torch.manual_seed(42)
+# this approach is equal to b = torch.randn(...).to(device) then use b.requires_grad_()
+b = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+w = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+print("b and w as gpu-tensors and trainable: ", b, w)
+
+# backward() will compute gradient for all "requires_grad" tensors: b, w, yhat, error
+yhat = b + w * x_train_tensor
+error = yhat - y_train_tensor
+loss = (error**2).mean() 
+loss.backward()
+print("Requires grad? ", error.requires_grad, yhat.requires_grad, b.requires_grad, w.requires_grad)
+print("Requires grad? ", y_train_tensor.requires_grad, x_train_tensor.requires_grad)
+# check values of the grads
+print("Grad values: ", b.grad, w.grad)
+
+# zeroing gradient after update
+b.grad.zero_() 
+w.grad.zero_()
+print("Gradients after zeroing: ", b.grad, w.grad)
+
+# training
+n_epochs = 1000
+optimizer = optim.SGD([b, w], lr=lr)
+loss_fn = nn.MSELoss(reduction='mean')
+for epoch in range(n_epochs):
+    yhat = b + w * x_train_tensor
+    ## Manual loss function
+    # error = yhat - y_train_tensor
+    # loss = (error**2).mean()
+    # loss.backward()
+    ## Manual updating and zeroing trainable parameters
+    # with torch.no_grad(): # to not losing gradient when re-compute b and w
+    #     b -= lr * b.grad
+    #     w -= lr * w.grad 
+    # b.grad.zero_()
+    # w.grad.zero_() 
+    ## Loss function with library instead of manual
+    loss = loss_fn(yhat, y_train_tensor)
+    loss.backward()
+    ## With optimizer instead of manual
+    optimizer.step()
+    optimizer.zero_grad()
+print("b and w after training: ", b, w)
+print("loss: ", loss)
+# convert loss to array
+print("loss numpy: ", loss.detach().cpu().numpy()) # detach() means removing gradient computation
+print("loss numpy (cleaner): ", loss.tolist()) # or loss.item() if one element only
+    
+# # visualize graph
+# b = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+# w = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+# yhat = b + w * x_train_tensor 
+# error = yhat - y_train_tensor 
+# loss = (error**2).mean()
+# dot = make_dot(yhat)
+# dot.format = 'png'
+# dot.render('test.png')
--- plots/chapter1.py
+++ plots/chapter1.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression 
 
+def fit_model(x_train, y_train):
+    regression = LinearRegression()
+    regression.fit(x_train, y_train)
+    b_minimum, w_minimum = regression.intercept_[0], regression.coef_[0][0]
+    return b_minimum, w_minimum
 
 def figure1(x_train, y_train, x_val, y_val):
     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
@@ -17,4 +22,18 @@ def figure1(x_train, y_train, x_val, y_val):
     ax[1].set_title('Generated Data - Validation')
     fig.tight_layout()
     return fig, ax
-    
+
+def figure3(x_train, y_train):
+    b_minimum, w_minimum = fit_model(x_train, y_train)
+    x_range = np.linspace(0, 1, 101)
+    yhat_range = b_minimum + w_minimum * x_range
+    fig, ax = plt.subplots(1, 1, figsize=(6,6))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_ylim([0, 3.1])
+    ax.scatter(x_train, y_train)
+    ax.plot(x_range, yhat_range, label='Final mode\'s predictions', c='k', linestyle='--')
+    ax.annotate('b={:.4f} w={:.4f}'.format(b_minimum, w_minimum), xy=(0.4, 1.4), c='k', rotation=34)
+    ax.legend(loc=0)
+    fig.tight_layout()
+    return fig, ax
