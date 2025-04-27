e2397e44f22e929a24ecd35bf16bab617e32d738
PHM1605 <PHM1605@gmail.com>
2025-04-28 00:54:06 +0700

    updates

--- chapter9.py
+++ chapter9.py
@@ -128,21 +128,21 @@ class EncoderDecoder(nn.Module):
 # encdec.eval() # switch the 'model.train' property to False
 # print("Naive prediction during evaluation: ", encdec(source_seq))
 
-# ## Data preparation
-# points, directions = generate_sequences(n=256, seed=13)
-# full_train = torch.as_tensor(np.array(points)).float() # [256,4,2]
-# target_train = full_train[:, 2:] # [256,2,2]
+## Data preparation
+points, directions = generate_sequences(n=256, seed=13)
+full_train = torch.as_tensor(np.array(points)).float() # [256,4,2]
+target_train = full_train[:, 2:] # [256,2,2]
 
-# test_points, test_directions = generate_sequences(seed=19)
-# full_test = torch.as_tensor(np.array(test_points)).float()
-# source_test = full_test[:, :2] #  [128,2,2]
-# target_test = full_test[:, 2:] # [128,2,2]
+test_points, test_directions = generate_sequences(seed=19)
+full_test = torch.as_tensor(np.array(test_points)).float()
+source_test = full_test[:, :2] #  [128,2,2]
+target_test = full_test[:, 2:] # [128,2,2]
 
-# train_data = TensorDataset(full_train, target_train)
-# test_data = TensorDataset(source_test, target_test)
-# generator = torch.Generator()
-# train_loader = DataLoader(train_data, batch_size=16, shuffle=True, generator=generator)
-# test_loader = DataLoader(test_data, batch_size=16)
+train_data = TensorDataset(full_train, target_train)
+test_data = TensorDataset(source_test, target_test)
+generator = torch.Generator()
+train_loader = DataLoader(train_data, batch_size=16, shuffle=True, generator=generator)
+test_loader = DataLoader(test_data, batch_size=16)
 
 # ## Model configuration and training 
 # torch.manual_seed(23)
@@ -161,7 +161,9 @@ class EncoderDecoder(nn.Module):
 # fig = sequence_pred(sbs_seq, full_test, test_directions)
 # plt.savefig('test.png')
 
-## Attention
+# ## Attention
+# fig = figure9()
+# plt.savefig('test.png')
 # Computing the context vector 
 full_seq = torch.tensor([[-1,-1],[-1,1],[1,1],[1,-1]]).float().view(1,4,2)
 source_seq = full_seq[:,:2] # [1,2,2]
@@ -191,20 +193,188 @@ context_vector = torch.bmm(alphas, values) # matrix-multiplication, [N,1,L]x[N,L
 concatenated = torch.cat([context_vector, query], axis=-1) # [N,1,2H]
 
 # Scoring method: to check if two hidden vectors are similar or not => using COSINE
+dims = query.size(-1)
 products = torch.bmm(query, keys.permute(0,2,1)) # [N,1,H]x[N,H,L]=[N,1,L]
-alphas = F.softmax(products, dim=-1) # [N,1,L] (sum each row is 1)
+scaled_products = products / np.sqrt(dims)
+alphas = F.softmax(scaled_products, dim=-1) # [N,1,L] (sum each row is 1)
+print("Alphas: ", alphas)
 
 def calc_alphas(ks, q):
+    dims = q.size(-1) # H
     products = torch.bmm(q, ks.permute(0,2,1)) # [N,1,H]x[N,H,L]=[N,1,L]
-    alphas = F.softmax(products, dim=-1)
+    scaled_products = products / np.sqrt(dims)
+    alphas = F.softmax(scaled_products, dim=-1) # [N,1,L]
     return alphas 
 
-dims = query.size(-1) # H
-scaled_products = products / np.sqrt(dims)
-
-# Visualizing the context Vector
+# Visualizing the context Vector with dummy q and k
 q = torch.tensor([0.55, 0.95]).view(1,1,2) # [N,1,H]
 k = torch.tensor([[0.65, 0.2], [0.85, -0.4], [-0.95, -0.75]]).view(1,3,2) # [N,L,H]
 fig = query_and_keys(q.squeeze(), k.view(3,2)) # [H], [L,H]
 plt.savefig('test.png')
+# [N,1,H]x[N,H,L]=[N,1,L]
+prod = torch.bmm(q, k.permute(0,2,1))
+print("How query 0th fits the keys of source sequence: ", prod)
+scores = F.softmax(prod, dim=-1)
+print("Softmax score of query 0th with keys of source sequence: ", scores)
+v = k 
+context = torch.bmm(scores, v) # [N,1,L]x[N,L,H]=[N,1,H]
+print("Context: ", context)
+fig = query_and_keys(q.squeeze(), k.view(3,2), context)
+plt.savefig('test.png')
 
+# Continue
+alphas = calc_alphas(keys, query)
+context_vector = torch.bmm(alphas, values) # [N,1,L]x[N,L,H]=[N,1,H]
+print("Context vector: ", context_vector)
+
+class Attention(nn.Module):
+    def __init__(self, hidden_dim, input_dim = None, proj_values=False):
+        super().__init__()
+        self.d_k = hidden_dim # dimension of key K, to scale the dot product between key and query
+        self.input_dim = hidden_dim if input_dim is None else input_dim 
+        self.proj_values = proj_values # to set we will project the value V or not
+
+        self.linear_query = nn.Linear(self.input_dim, hidden_dim)
+        self.linear_key = nn.Linear(self.input_dim, hidden_dim)
+        self.linear_value = nn.Linear(self.input_dim, hidden_dim)
+        self.alphas = None 
+    
+    # keys: [N,L,H]
+    def init_keys(self, keys):
+        self.keys = keys 
+        self.proj_keys = self.linear_key(self.keys)
+        self.values = self.linear_value(self.keys) if self.proj_values else self.keys
+    
+    def score_function(self, query):
+        proj_query = self.linear_query(query)
+        # [N,1,H]x[N,H,L]->[N,1,L]
+        dot_products = torch.bmm(proj_query, self.proj_keys.permute(0,2,1))
+        scores = dot_products / np.sqrt(self.d_k)
+        return scores # [N,1,L]
+
+    def forward(self, query, mask=None):
+        # query: [N,1,L]; scores: [N,1,H]
+        scores = self.score_function(query)
+        if mask is not None:            
+            # set scores of padding sample [0,0] to minus infinity
+            scores = scores.masked_fill(mask==0, -1e9) # [N,1,L]=[[[False,True]]]
+        alphas = F.softmax(scores, dim=-1) # [N,1,L]
+        self.alphas = alphas.detach()
+        # [N,1,L]x[N,L,H]->[N,1,H]
+        context = torch.bmm(alphas, self.values)
+        return context 
+
+## Source mask - mask some (padded) values V to ignore them
+source_seq = torch.tensor([[[-1,1], [0,0]]]) # [N,L,H]=[1,2,2]
+# ..assuming there is an encoder here
+keys = torch.tensor([[[-0.38, 0.44], [0.85, -0.05]]]) # [N,L,H]
+query = torch.tensor([[[-1., 1.]]]) # [N,1,H]=[1,1,2]
+source_mask = (source_seq != 0).all(axis=2).unsqueeze(1) # [N,1,L]
+print("Source mask: ", source_mask) # [[[True, False]]]
+torch.manual_seed(11)
+attnh = Attention(2) # hidden_dim = input_dim = 2
+attnh.init_keys(keys) # init key and value
+context = attnh(query, mask=source_mask)
+print("Attention alphas: ", attnh.alphas)
+
+class DecoderAttn(nn.Module):
+    def __init__(self, n_features, hidden_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim 
+        self.n_features = n_features 
+        self.hidden = None # hidden final from Encoder
+        self.basic_rnn = nn.GRU(self.n_features, self.hidden_dim, batch_first=True)
+        self.attn = Attention(self.hidden_dim)
+        # regression just before the Decoder's output
+        self.regression = nn.Linear(2*self.hidden_dim, self.n_features)
+    
+    # hidden_seq: [N,L,H]
+    def init_hidden(self, hidden_seq):
+        self.attn.init_keys(hidden_seq) # init & project key and value
+        hidden_final = hidden_seq[:, -1:] # [N,1,H]
+        self.hidden = hidden_final.permute(1,0,2) # [L,N,H] with L=1
+
+    # X: [N,1,F]
+    def forward(self, X, mask=None):
+        # [N,1,H] for both
+        batch_first_output, self.hidden = self.basic_rnn(X, self.hidden)
+        query = batch_first_output[:, -1:]
+        context = self.attn(query, mask=mask) # [N,1,H]
+        concatenated = torch.cat([context, query], axis=-1) # [N,1,2*H]
+        out = self.regression(concatenated) # [N,1,n_features]
+        return out.view(-1,1,self.n_features)
+
+full_seq = torch.tensor([[-1,-1],[-1,1],[1,1],[1,-1]]).float().view(1,4,2)
+source_seq = full_seq[:,:2] # [N,L,F]=[1,2,2]
+target_seq = full_seq[:,2:] # [1,2,2]
+torch.manual_seed(21)
+encoder = Encoder(n_features=2, hidden_dim=2)
+decoder_attn = DecoderAttn(n_features=2, hidden_dim=2)
+hidden_seq = encoder(source_seq)
+decoder_attn.init_hidden(hidden_seq)
+# Target sequence generation
+inputs = source_seq[:,-1:] # [N,L,H]=[1,1,2]
+target_len = 2 
+for i in range(target_len):
+    out = decoder_attn(inputs)
+    print(f'Output: {out}')
+    inputs = out 
+
+## Encoder + Decoder + Attention
+encdec = EncoderDecoder(encoder, decoder_attn, 
+    input_len=2, target_len=2, teacher_forcing_prob=0.0)
+print("Encoder Decoder Attention result: ", encdec(full_seq))
+
+## Still, create a class inheriting from above, but with storing attention scores
+class EncoderDecoderAttn(EncoderDecoder):
+    def __init__(self, encoder, decoder, input_len, target_len, teacher_forcing_prob=0.5):
+        super().__init__(encoder, decoder, input_len, target_len, teacher_forcing_prob)
+        self.alphas = None 
+    
+    # overwrite the "init_outputs" and "store_output"
+    def init_outputs(self, batch_size):
+        device = next(self.parameters()).device
+        # [N,L(target),F] 
+        self.outputs = torch.zeros(
+            batch_size, self.target_len, self.encoder.n_features
+        ).to(device)
+        # [N,L(target),L(source)]
+        self.alphas = torch.zeros(
+            batch_size, self.target_len, self.input_len
+        ).to(device)
+    
+    def store_output(self, i, out):
+        self.outputs[:, i:i+1, :] = out
+        self.alphas[:, i:i+1, :] = self.decoder.attn.alphas 
+
+torch.manual_seed(17)
+encoder = Encoder(n_features=2, hidden_dim=2)
+decoder_attn = DecoderAttn(n_features=2, hidden_dim=2)
+model = EncoderDecoderAttn(encoder, decoder_attn, input_len=2, target_len=2, teacher_forcing_prob=0.5)
+loss = nn.MSELoss() 
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+sbs_seq_attn = StepByStep(model, loss, optimizer)
+sbs_seq_attn.set_loaders(train_loader, test_loader)
+sbs_seq_attn.train(100)
+fig = sbs_seq_attn.plot_losses()
+plt.savefig('test.png')
+
+# Visualizing Predictions
+fig = sequence_pred(sbs_seq_attn, full_test, test_directions)
+plt.savefig('test.png')
+
+# Visualizing Attention
+# First sample
+inputs = full_train[:1, :2] # [1,2,2]
+out = sbs_seq_attn.predict(inputs)
+print("Attention score of first sample:\n", sbs_seq_attn.model.alphas)
+# First 10 samples 
+inputs = full_train[:10, :2] # [10,2,2]
+source_labels = ['Point #1', 'Point #2']
+target_labels = ['Point #3', 'Point #4']
+point_labels = [
+    f'{"Counter-" if not directions[i] else ""}Clockwise\nPoint #1: {inp[0,0]:.2f},{inp[0,1]:.2f}'
+    for i, inp in enumerate(inputs)
+]
+fig = plot_attention(model, inputs, point_labels, source_labels, target_labels)
+plt.savefig('test.png')
--- plots/chapter9.py
+++ plots/chapter9.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt 
+import matplotlib 
 import numpy as np 
 
 def add_arrow(line, position=None, direction='right', size=15, color=None, lw=2, alpha=1.0, text=None, text_offset=(0,0)):
@@ -68,6 +69,7 @@ def make_line(ax, point):
     return line
 
 # q: [H], ks: [L,H] with L=sequence length, H=hidden size
+# result: one point
 def query_and_keys(q, ks, result=None, ax=None):
     if ax is None:
         fig, ax = plt.subplots(1, 1, figsize=(5,5))
@@ -80,16 +82,111 @@ def query_and_keys(q, ks, result=None, ax=None):
     cos_k = []
     for k in ks:
         line_k.append(make_line(ax, k))
-    norm_k.append(np.linalg.norm(k))
-    cos_k.append(np.dot(q, k)/(norm_k[-1]*norm_q))
+        norm_k.append(np.linalg.norm(k))
+        cos_k.append(np.dot(q, k)/(norm_k[-1]*norm_q))
     add_arrow(line_q, lw=2, color='r', text=f'||Q||={norm_q:.2f}', size=12)
-    add_arrow(line_k[0], lw=2, color='k', text=f'')
+    add_arrow(line_k[0], lw=2, color='k', text=r'$||K_0'+f'||={norm_k[0]:.2f}$'+'\n'+r'$cos\theta_0='+f'{cos_k[0]:.2f}$', size=12, text_offset=(-0.33,0.1))
+    add_arrow(line_k[1], lw=2, color='k', text=r'$||K_1'+f'||={norm_k[1]:.2f}$'+'\n'+r'$cos\theta_1='+f'{cos_k[1]:.2f}$', size=12, text_offset=(-0.63,-0.18))
+    add_arrow(line_k[2], lw=2, color='k', text=r'$||K_2'+f'||={norm_k[2]:.2f}$'+'\n'+r'$cos\theta_2='+f'{cos_k[2]:.2f}$', size=12, text_offset=(.05, .58))
+
+    if result is not None:
+        add_arrow(make_line(ax, result), lw=2, color='g', text=f'Context Vector', size=12, text_offset=(-0.26, 0.1))
+
+    circle1 = plt.Circle((0,0), 1., color='k', fill=False, lw=2)
+    ax.add_artist(circle1)
+
+    ax.set_xticks([-1.0, 0, 1.0])
+    ax.set_xticklabels([-1.0, 0, 1.0], fontsize=12)
+    ax.set_yticks([-1.0, 0, 1.0])
+    ax.set_yticklabels([-1.0, 0, 1.0], fontsize=12)
 
     ax.set_ylim([-1.02, 1.02])
     ax.set_xlim([-1.02, 1.02])
+
     ax.set_xlabel(r'$x_0$')
     ax.set_ylabel(r'$x_1$')
     ax.set_title(r'$Query\ and\ Keys$')
     fig.tight_layout()
     return fig
 
+def plot_attention(model, inputs, point_labels=None, source_labels=None, target_labels=None, decoder=False, self_attn=False, n_cols=5, alphas_attr='alphas'):
+    text_colors = ["white", "black"]
+    kw = dict(horizontalalignment="center", verticalalignment="center")
+    valfmt = matplotlib.ticker.StrMethodFormatter("{x:.2f}")
+    model.eval()
+    device = list(model.parameters())[0].device.type
+    predicted_seqs = model(inputs.to(device))
+    for attr in alphas_attr.split('.'): # 'alphas'
+        alphas = getattr(model, attr)
+    if len(alphas.shape) < 4: # [N,L(target),L(source)]=[10,2,2]=>[1,10,2,2]
+        alphas = alphas.unsqueeze(0)
+    alphas = np.array(alphas.tolist())
+    n_heads, n_points, target_len, input_len = alphas.shape
+
+    if point_labels is None:
+        point_labels = [f'Point #{i}' for i in range(n_points)]
+    
+    if source_labels is None:
+        source_labels = [f'Input #{i}' for i in range(input_len)]
+    
+    if target_labels is None:
+        target_labels = [f'Target #{i}' for i in range(target_len)]
+    
+    if n_heads == 1:
+        n_rows = (n_points//n_cols) + int((n_points%n_cols) > 0)
+    else:
+        n_cols = n_heads 
+        n_rows = n_points
+    
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(n_cols*3, n_rows*3))
+    for i in range(n_points):
+        for head in range(n_heads):
+            data = alphas[head][i].squeeze()
+            if n_heads > 1:
+                if n_points > 1:
+                    ax = axs[i, head]
+                else:
+                    ax = axs[head]
+            else:
+                ax = axs.flat[i]
+            im = ax.imshow(data, vmin=0, vmax=1, cmap=plt.cm.gray)
+            ax.grid(False)
+            
+
+    fig.subplots_adjust(wspace=0.8, hspace=1.0)
+    fig.tight_layout()
+    return fig 
+
+def figure9():
+    english = ['the', 'European', 'economic', 'zone']
+    french = ['la', 'zone', 'économique', 'européenne']
+    source_labels = english 
+    target_labels = french 
+    data = np.array([
+        [0.8, 0, 0, 0.2],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 0.8, 0, 0.2]
+        ])
+    fig, ax = plt.subplots(1, 1, figsize=(5,5))
+    # color the cells
+    im = ax.imshow(data, vmin=0, vmax=1, cmap=plt.cm.gray)
+    ax.grid(False)
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    ax.set_xticklabels(source_labels, rotation=90)
+    ax.set_yticklabels(target_labels)
+    # move x label from bottom to top
+    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+
+    textcolors = ["white", "black"]
+    kw = dict(horizontalalignment="center", verticalalignment="center") # set text alignment in each cell in axis
+    valfmt = matplotlib.ticker.StrMethodFormatter("{x:.2f}") # format the displayed text (number) in each cell in axis
+    threshold = im.norm(data.max())/2
+    for ip in range(data.shape[0]):
+        for jp in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[ip,jp])) > threshold])
+            text = im.axes.text(jp, ip, valfmt(data[ip,jp],None), **kw)
+
+    fig.tight_layout()
+    return fig 
